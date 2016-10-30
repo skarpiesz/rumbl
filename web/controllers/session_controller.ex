908c2427418af2019d4784341fe19f26105051bd
Serge Karpiesz <skarpiesz@gmail.com>
2016-10-30 19:47:11 +0300

    Presenting user account links

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -12,4 +12,8 @@ defmodule Rumbl.SessionController do
         conn |> put_flash(:error, "Invalid username/password combination") |> render("new.html")
     end
   end
+
+  def delete(conn, _) do
+    conn |> Rumbl.Auth.logout |> redirect(to: page_path(conn, :index)) 
+  end
 end
