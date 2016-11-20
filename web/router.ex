f5f8cdbffb0f6eebbdf7bcb04bda350bd9967969
Serge Karpiesz <skarpiesz@gmail.com>
2016-11-20 19:46:11 +0300

    Watching videos

--- web/router.ex
+++ web/router.ex
@@ -20,6 +20,8 @@ defmodule Rumbl.Router do
     resources "/users", UserController, only: [:index, :show, :new, :create]
     resources "/sessions", SessionController, only: [:new, :create, :delete]
     resources "/videos", VideoController
+    get "/watch/:id", WatchController, :show
+
     get "/", PageController, :index
   end
 
