f5897d7d6d3d5094c65f43980a617b386b44e799
Serge Karpiesz <skarpiesz@gmail.com>
2016-11-22 21:55:56 +0300

    Phoenix clients with ES6

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,6 +1,11 @@
 alias Rumbl.Repo
 alias Rumbl.Category
+alias Rumbl.User
 
 for category <- ~w(Action Drama Romance Comedy Sci-fi) do
-  Repo.get_by(Category, name: category) || Repo.insert!(%Category{name: category}) 
+  Repo.get_by(Category, name: category) || Repo.insert!(%Category{name: category})
+end
+
+for user_params <- [%{name: "Serge Karpiesz", username: "skarpiesz", password: "123123123"}] do
+  Repo.get_by(User, username: "skarpiesz") || Repo.insert!(User.registration_changeset(%User{}, user_params))
 end
--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,10 +1,5 @@
 import "phoenix_html"
-import Player from "./player"
+import socket from "./socket"
+import Video from "./video"
 
-let video = document.getElementById("video")
-
-if (video) {
-  Player.init(video.id, video.getAttribute("data-player-id"), () => {
-    console.log("player ready!")
-  })
-}
+Video.init(socket, document.getElementById("video"))
