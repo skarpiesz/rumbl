db131ab70bdfc526173fdfb909abceb589f10e72
Serge Karpiesz <skarpiesz@gmail.com>
2016-12-02 21:33:53 +0300

    Socket authentication

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -12,16 +12,16 @@ defmodule Rumbl.Auth do
 
     cond do
       user = conn.assigns[:current_user] ->
-        conn
+        put_current_user(conn, user)
       user = user_id && repo.get(Rumbl.User, user_id) ->
-        assign(conn, :current_user, user)
+        put_current_user(conn, user)
       true ->
         assign(conn, :current_user, nil)
     end
   end
 
   def login(conn, user) do
-    conn |> assign(:current_user, user) |> put_session(:user_id, user.id) |> configure_session(renew: true)
+    conn |> put_current_user(user) |> put_session(:user_id, user.id) |> configure_session(renew: true)
   end
 
   def login_by_username_and_pass(conn, username, given_pass, opts) do
@@ -46,5 +46,10 @@ defmodule Rumbl.Auth do
       conn |> put_flash(:error, "You must be logged in to access that page")
            |> redirect(to: Helpers.page_path(conn, :index)) |> halt
     end
+  end
+
+  defp put_current_user(conn, user) do
+    token = Phoenix.Token.sign(conn, "user socket", user.id)
+    conn |> assign(:current_user, user) |> assign(:user_token, token)
   end
 end
