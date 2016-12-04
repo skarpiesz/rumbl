4637a42c9409b7d6ef21309693ff6233b6bd21a3
Serge Karpiesz <skarpiesz@gmail.com>
2016-12-05 00:56:02 +0300

    Building GenServers for OTP

--- lib/rumbl.ex
+++ lib/rumbl.ex
@@ -8,16 +8,11 @@ defmodule Rumbl do
 
     # Define workers and child supervisors to be supervised
     children = [
-      # Start the Ecto repository
       supervisor(Rumbl.Repo, []),
-      # Start the endpoint when the application starts
       supervisor(Rumbl.Endpoint, []),
-      # Start your own worker by calling: Rumbl.Worker.start_link(arg1, arg2, arg3)
-      # worker(Rumbl.Worker, [arg1, arg2, arg3]),
+      worker(Rumbl.Counter, [5], restart: :temporary)
     ]
 
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Rumbl.Supervisor]
     Supervisor.start_link(children, opts)
   end
